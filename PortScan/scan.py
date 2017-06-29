957cd15be9b657deccd743da07646aa4ba317246
Jessee <1737723363@qq.com>
2017-06-29 18:41:14 +0800

    完成

---
+++ PortScan/scan.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+import socket
+from threading import Thread
+
+import time
+
+result = []
+
+
+def scaner(host, port):
+	sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sockfd.settimeout(0.1)
+	print(f'正在检测{host}:{port}...')
+	tryConn = sockfd.connect_ex((host, port))
+	if not tryConn:  # 返回0表示连接成功
+		result.append(port)
+	else:
+		sockfd.close()
+
+
+if __name__ == '__main__':
+	host = input('请输入IP地址：')
+	while True:
+		startPort = int(input('请输入起始扫描端口：'))
+		endPort = int(input('请输入终止扫描端口：'))
+		if startPort >= 65536 or endPort >= 65536:
+			print('端口号不能大于65535')
+		else:
+			break
+	startTime = time.time()
+	threads = []
+	for p in range(startPort, endPort + 1):
+		t = Thread(target=scaner, args=(host, p))
+		threads.append(t)
+		t.start()
+	for t in threads:
+		t.join()
+	print('端口扫描完毕，端口开放结果如下')
+	result.sort()
+	for p in result:
+		print(f'{host}:{p}')
+	endTime = time.time()
+	print(f'本次端口扫描用时{endTime-startTime}秒')
